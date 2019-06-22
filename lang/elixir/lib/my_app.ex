0d1ec4bfc7ed535437d7aabb91f8de8f603f03d1
julio-jimenez <juliofrancisj@gmail.com>
2019-06-21 20:05:21 -0400

    remove other demos

--- lang/elixir/lib/my_app.ex
+++
@@ -1,7 +0,0 @@
-defmodule MyApp do
-  use Application
-
-  def start(_type, _args) do
-    Plug.Adapters.Cowboy.http(MyApp.Router, [], [port: 8080])
-  end
-end
--- lang/swift/Package.swift
+++
@@ -1,8 +0,0 @@
-import PackageDescription
-
-let package = Package(
-    name: "MyApplication",
-    dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 6)
-    ]
-)
